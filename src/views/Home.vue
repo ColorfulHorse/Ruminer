4fb0e4b9f0a2059e185aa18be94e53298105cf95
xumou <441008784@qq.com>
2020-07-17 01:17:11 +0800

    更新UI  home   main   更改了logo图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <el-container id="home">
-        <el-aside>
+        <el-aside >
             <el-menu
+                    @select="menuSelect"
                     :default-active="$route.path"
                     :background-color="colors.menuBg"
                     :text-color="colors.menuText"
@@ -11,8 +12,9 @@
                         v-for="item in $router.options.routes.find((value) => value.name==='Home').children"
                         :key="item.id"
                         :index="item.path">
-                    <i :class="item.meta.icon"></i>
+                  <div :class="currentIndex===item.path?'el-menu-item-wrapper-checked':'el-menu-item-wrapper-checkable'">
                     <span slot="title">{{item.meta.name}}</span>
+                  </div>
                 </el-menu-item>
                 <!--                <el-menu-item index="main">-->
                 <!--                    <i class="el-icon-house"></i>-->
@@ -47,8 +49,16 @@ import colors from '@/styles/colors.scss'
 export default class Home extends Vue {
   colors = colors
 
-  created() {
-    this.$router.push('main')
+  currentIndex= ''
+
+  data() {
+    return {
+      currentIndex: '/main' // 默认给的是哪个就给哪个的path
+    }
+  }
+
+  menuSelect(item:string) {
+    this.currentIndex = item
   }
 }
 </script>
@@ -59,15 +69,32 @@ export default class Home extends Vue {
         width: 100%;
 
         .el-aside {
+          background: #F1F1F1;
             width: 25% !important;
             height: 100%;
             .el-menu {
-                height: 100%;
+              height: 100%;
+              width: 100%;
             }
         }
-
         .el-menu-item {
-            padding: 0 15% !important;
+            padding:0 !important;
+            .el-menu-item-wrapper-checkable{
+              width: 100%;
+              background: #F1F1F1;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+            .el-menu-item-wrapper-checked{
+              width: 100%;
+              background: #FFFFFF;
+              display: flex;
+              font-weight: 700;
+              justify-content: center;
+              align-items: center;
+              border-top: 1px solid #DCDCDC;
+            }
 
             [class^="el-icon-"] {
                 margin-right: 16px;
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,20 +1,34 @@
 <template>
-    <el-main>
-        <el-row :gutter="10">
-          <el-col :span="10">
-            <div class="action-wrapper">
-              <p class="action">捕获屏幕</p>
-              <p class="tips">快捷键:XXX</p>
-            </div>
-          </el-col>
-          <el-col :span="10">
-            <div class="action-wrapper">
-              <p class="action">捕获窗口</p>
-              <p class="tips">快捷键:XXX</p>
-            </div>
-          </el-col>
-        </el-row>
-    </el-main>
+  <el-main>
+    <el-row>
+      <el-col :span="24">
+        <div class="sf-wrapper">
+          <el-image class="sf-logo" fit="contain" lazy :src="require('../../public/tray/logo.png')"/>
+          <div class="sf-name">Auto-Translator</div>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col :span="8">
+        <div class="action-wrapper">
+          <p class="action">捕获屏幕</p>
+          <p class="tips">XXX</p>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="action-wrapper">
+          <p class="action">捕获窗口</p>
+          <p class="tips">XXX</p>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="action-wrapper">
+          <p class="action">试试按键</p>
+          <p class="tips">XXX</p>
+        </div>
+      </el-col>
+    </el-row>
+  </el-main>
 </template>
 
 <script lang="ts">
@@ -23,9 +37,10 @@ import { Component, Vue } from 'vue-property-decorator'
 import { ipcRenderer } from 'electron'
 import { IPC } from '../constant/Constants'
 
-@Component
+  @Component
 export default class Main extends Vue {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
+
   selectArea() {
     ipcRenderer.send(IPC.SELECT_AREA)
   }
@@ -34,23 +49,55 @@ export default class Main extends Vue {
 
 <style scoped lang="scss">
   .el-row {
+    .sf-wrapper {
+      width: 100%;
+      height: 100px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+
+      .sf-logo {
+        width: 60px;
+        height: 60px;
+        margin-right: 20px;
+      }
+
+      .sf-name {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        color: #2C3E50;
+        font-size: 32px;
+        font-weight: 400;
+      }
+    }
+
     .el-col {
-      .action-wrapper{
-        background-color: beige;
+      .action-wrapper {
+        background-color: #F1F1F1;
         border-radius: 0 5px 0 5px;
         padding: 10px;
         text-align: center;
         vertical-align: middle;
       }
-        .action {
-          font-size: 18px;
-          color: black;
-        }
-        .tips {
-          font-size: 16px;
-          color: gray;
-          margin-top: 10px;
-        }
+
+      .action {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-size: 17px;
+        font-weight: 700;
+        color: black;
+      }
+
+      .tips {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-size: 13px;
+        font-weight: 100;
+        color: gray;
+        margin-top: 10px;
+      }
     }
   }
 </style>
